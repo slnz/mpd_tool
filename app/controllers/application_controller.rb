86d2256cc34cdbcbd5092126836555c2ba969cc8
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-21 13:08:28 +1300

    added admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_admin_user!
+    fail SecurityError unless current_user.try(:admin?)
+  rescue SecurityError
+    flash[:error] = 'You are not an Admin'
+    redirect_to root_path
+  end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,7 +55,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  config.authentication_method = :authenticate_user!
+  config.authentication_method = :authenticate_admin_user!
 
   # == User Authorization
   #
@@ -83,7 +83,6 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
   config.current_user_method = :current_user
-
 
   # == Logging Out
   #
