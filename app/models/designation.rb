a4e056f13e4bc0e32a1e8ad421b7beaac02486e7
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-07-13 15:30:16 +1200

    designation email fixes

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -5,15 +5,13 @@ class Designation < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :activation_code, presence: true
   before_validation :generate_activation_code, on: :create
-  after_create :send_activation_code
+  before_save :send_activation_code, on: :create
   belongs_to :user
   has_many :donations, primary_key: :designation_code
 
   def send_activation_code
-    generate_activation_code unless activation_code
     DesignationMailer.send_activation_code(self).deliver
     self.email_sent = true
-    save
   end
 
   protected
---
+++ db/schema.rb
@@ -0,0 +1,96 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141020233955) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "email"
+    t.integer  "code"
+    t.integer  "designation_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "designations", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "designation_code"
+    t.string   "activation_code"
+    t.boolean  "email_sent",       default: false
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "designations", ["activation_code"], name: "index_designations_on_activation_code", unique: true, using: :btree
+
+  create_table "donations", force: true do |t|
+    t.integer  "global_id"
+    t.integer  "contact_id"
+    t.integer  "designation_id"
+    t.string   "payment_method"
+    t.date     "display_date"
+    t.decimal  "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "project_id"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.integer  "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",              default: "",    null: false
+    t.string   "encrypted_password", default: "",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.integer  "designation_id"
+    t.string   "activation_code"
+    t.boolean  "admin",              default: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+end
