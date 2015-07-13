531ff1c3a87fa046b9e78cab6e287ac01f22e1bf
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-07-13 15:36:13 +1200

    undo

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -5,13 +5,15 @@ class Designation < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :activation_code, presence: true
   before_validation :generate_activation_code, on: :create
-  before_save :send_activation_code, on: :create
+  after_save :send_activation_code, on: :create
   belongs_to :user
   has_many :donations, primary_key: :designation_code
 
   def send_activation_code
+    generate_activation_code unless activation_code
     DesignationMailer.send_activation_code(self).deliver
     self.email_sent = true
+    save unless persisted?
   end
 
   protected
