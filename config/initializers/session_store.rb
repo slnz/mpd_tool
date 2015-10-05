b49821b1c6419ed21baea6ee11eb4fd8fbaab390
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-10-05 16:14:53 +1300

    unified cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mpd-sl_session'
+Rails.application.config.session_store :cookie_store, key: '_mpd_slnz_session', domain: {
+  production: '.studentlife.org.nz',
+  development: '.lvh.me'
+}.fetch(Rails.env.to_sym, :all)
