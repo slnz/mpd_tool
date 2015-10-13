77b39c3ed2d2a01a2510bbb444673aeaddc9034a
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-10-13 17:57:07 +1300

    partner view fix

--- app/mailers/give/donors_mailer.rb
+++ app/mailers/give/donors_mailer.rb
@@ -2,14 +2,14 @@ module Give
   class DonorsMailer < GiveMailer
     def welcome(donor)
       @donor = donor.try(:decorate)
-      mail to: donor.email, subject: 'Thanks for Partnering with Summer Projects'
+      mail to: donor.email, subject: 'Thanks for partnering with Summer Projects'
     end
 
     def thanks_for_pledge(pledge)
       @donee = pledge.donee.decorate
       @donor = pledge.donor.decorate
       @pledge = pledge.decorate
-      mail to: donor.email, subject: 'Thanks for Partnering with Summer Projects'
+      mail to: donor.email, subject: 'Thanks for partnering with Summer Projects'
     end
   end
 end
