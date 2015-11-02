d05de278b14d59ec92140641c1ec1c7adbcc7088
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-11-02 21:54:26 +1300

    allow duplicate nil codes on contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   has_many :donations, dependent: :destroy
-  validates :code, uniqueness: true
+  validates :code, uniqueness: true, allow_nil: true
   validates :name, presence: true
 end
