73f667b9878300ceeb54b62e84149b5d93e806e7
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-12-07 13:59:13 +1300

    omniauth fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,45 @@ class User < ActiveRecord::Base
   validates :email, presence: true, unless: :uid
   before_save :set_name
 
-  def self.from_omniauth(auth)
-    where(provider: auth[:provider], uid: auth[:uid]).first_or_create(
-      email: auth[:info][:email] || "#{auth[:uid]}@#{auth[:provider]}.com",
-      first_name: auth[:info][:first_name],
-      last_name: auth[:info][:last_name],
-      image: auth[:info][:image]
-    )
-  end
-
   def set_name
     self.name = "#{first_name} #{last_name}".strip
   end
 
   def reference_name
     "#{first_name[0]} #{last_name}".strip.upcase
+  end
+
+  def self.from_omniauth(auth)
+    where('(provider = ? AND uid = ?) OR email = ?',
+          auth[:provider],
+          auth[:uid],
+          auth[:info][:email] || "#{auth[:uid]}@#{auth[:provider]}.com")
+      .first_or_initialize.assign_omniauth_attributes(auth)
+  end
+
+  def assign_omniauth_attributes(auth)
+    assign_new_omniauth_attributes(auth) if new_record?
+    assign_new_and_existing_omniauth_attributes(auth)
+    save
+    self
+  end
+
+  protected
+
+  def assign_new_omniauth_attributes(auth)
+    assign_attributes(
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      email: auth[:info][:email] || "#{auth[:uid]}@#{auth[:provider]}.com",
+      password: Devise.friendly_token[0, 20],
+      image: auth[:info][:image]
+    )
+  end
+
+  def assign_new_and_existing_omniauth_attributes(auth)
+    assign_attributes(
+      provider: auth[:provider],
+      uid: auth[:uid]
+    )
   end
 end
